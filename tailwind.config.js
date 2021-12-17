320dd70c674c77c028b54f87b038e51b81c39440
Rahman <rahmandevbd@gmail.comm>
2021-12-17 13:11:05 +0600

    single blog add complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,18 @@ module.exports = {
       backgroundImage: {
         "slider-img-1": "url('../src/img/bg.png')",
         "slider-img-2": "url('../src/img/bg-.jpg')",
+        // blog image
+        blog_img_1: "url('../src/img/blog.jpg')",
       },
-
       colors: {
         theme_overlay: "#00000080",
         accent_color: "#00FF29",
       },
       screens: {
         xsm: "320",
+      },
+      fontFamily: {
+        roboto: ['"Roboto",sans-serif'],
       },
     },
   },
