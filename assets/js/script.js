fb5e584c0338f825d0510999a2bcaffc3cfa9e3d
rahman <rahmandevbe@gmail.com>
2021-12-23 23:30:53 +0600

    Rearange File Index and add atoplay in hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     items: 1,
     loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
   });
 });
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,8 @@ module.exports = {
         center: true,
       },
       backgroundImage: {
-        "slider-img-1": "url('../src/img/bg.png')",
-        "slider-img-2": "url('../src/img/bg-.jpg')",
-        // blog image
-        blog_img_1: "url('../src/img/blogs/blog.jpg')",
-        blog_img_2: "url('../src/img/blogs/blog-2.jpg')",
-        blog_img_3: "url('../src/img/blogs/blog-3.jpg')",
-        blog_img_4: "url('../src/img/blogs/blog-4.jpg')",
+        "slider-img-1": "url('../img/bg.png')",
+        "slider-img-2": "url('../img/bg-.jpg')",
       },
       colors: {
         theme_overlay: "#00000080",
