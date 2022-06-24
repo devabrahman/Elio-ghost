d34d0366aa9bb4e15bb2e8a3656a011b6a454910
Abdur Rahman <rhmandevbd@gmail.com>
2022-06-24 21:47:36 +0600

    commint everyhing okay

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,15 +8,15 @@ $(document).ready(function () {
     animateIn: "fadeIn",
     animateOut: "fadeOut",
   });
-});
+  // });
 
-// Hamberger Menu
-const hambergur = document.querySelector(".hambarger_nav");
-const hambergurClose = document.querySelector(".hambarger_nav > i");
+  // // Hamberger Menu
+  // const hambergur = document.querySelector(".hambarger_nav");
+  // const hambergurClose = document.querySelector(".hambarger_nav > i");
 
-const nave_menu = document.querySelector(".nav_menu");
-hambergur.addEventListener("click", () => {
-  nave_menu.classList.toggle("side_nav");
-  hambergur.classList.toggle("nav_open");
-  hambergurClose.classList.toggle("fa-times");
+  // const nave_menu = document.querySelector(".nav_menu");
+  // hambergur.addEventListener("click", () => {
+  //   nave_menu.classList.toggle("side_nav");
+  //   hambergur.classList.toggle("nav_open");
+  //   hambergurClose.classList.toggle("fa-times");
 });
