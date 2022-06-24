fe29c84b4dd7fa2103994f7c9cf0f83d4735e7f5
Abdur Rahman <rhmandevbd@gmail.com>
2022-06-24 21:47:17 +0600

    a long update wich is parfect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  mode: "jit",
-  purge: ["*.html"],
-  darkMode: "media", // or 'media' or 'class'
-  theme: {
-    extend: {
-      container: {
-        center: true,
-      },
-      backgroundImage: {
-        "slider-img-1": "url('../img/bg.png')",
-        "slider-img-2": "url('../img/bg-.jpg') ",
-      },
-      colors: {
-        theme_overlay: "#00000080",
-        accent_color: "#00FF29",
-      },
-      screens: {
-        xsm: "320",
-      },
-      fontFamily: {
-        roboto: ['"Roboto",sans-serif'],
-        roboto: ['"Roboto",sans-serif'],
-      },
+    mode: "jit",
+    purge: ["*.hbs"],
+    darkMode: "media", // or 'media' or 'class'
+    theme: {
+        extend: {
+            container: {
+                center: true,
+            },
+            backgroundImage: {
+                "slider-img-1": "url('../img/bg.png')",
+                "slider-img-2": "url('../img/bg-.jpg') ",
+            },
+            colors: {
+                theme_overlay: "#00000080",
+                accent_color: "#00FF29",
+            },
+            screens: {
+                xsm: "320",
+            },
+            fontFamily: {
+                roboto: ['"Roboto",sans-serif'],
+                roboto: ['"Roboto",sans-serif'],
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
