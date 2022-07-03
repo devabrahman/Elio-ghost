51ed505d82809189ccc1fafd7e78ee566c750bbe
Abdur Rahman <rhmandevbd@gmail.com>
2022-07-03 17:45:02 +0600

    taildwind intrigration :)

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,39 @@ $(document).ready(function () {
   //   hambergur.classList.toggle("nav_open");
   //   hambergurClose.classList.toggle("fa-times");
 });
+
+  // Load more
+    var pagination_next_url = document.querySelector('link[rel=next]').getAttribute('href'),
+        $load_posts_button  = document.querySelector('.it-load-posts');
+
+    $load_posts_button.click(function(e) {
+        e.preventDefault();
+
+        var request_next_link = pagination_next_url.split(/page/)[0] + 'page/' + pagination_next_page_number + '/';
+
+        $.ajax({
+        url: request_next_link,
+        beforeSend: function() {
+            $load_posts_button.text('{{t "Loading"}}');
+            $load_posts_button.addClass('loading');
+        }
+        }).done(function(data) {
+            var posts = $('.post', data);
+
+            $load_posts_button.text('{{t "Load More"}}');
+            $load_posts_button.removeClass('loading');
+
+                console.log(posts)
+            $('.it-postfeed').append(posts);
+
+           
+
+            pagination_next_page_number++;
+
+            // If you are on the last pagination page, add the disabled attribute
+            if (pagination_next_page_number > pagination_available_pages_number) {
+                $load_posts_button.attr('disabled', true);
+            }
+        });
+        
+    });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mode: "jit",
-    purge: ["*.hbs"],
+    purge: ["*.hbs","partials/*.hbs"],
     darkMode: "media", // or 'media' or 'class'
     theme: {
         extend: {
